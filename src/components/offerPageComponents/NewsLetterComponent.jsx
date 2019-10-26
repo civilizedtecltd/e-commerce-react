a4acf99cb7dc43f22c44c6b0c78c62eecd60d4ef
tawhidjony <tawhid.jony.jhm@gmail.com>
2019-10-26 10:09:47 +0600

    complete offer page

---
+++ src/components/offerPageComponents/NewsLetterComponent.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Row, Col, Card,Form , Button} from 'react-bootstrap';
+
+function NewsLetterComponent() {
+    return (
+        <Row className="justify-content-center">
+        <Col sm="10">
+          <Card className="subscribeCard border-0 rounded-0">
+            <Card.Body className="text-center pt-5 pb-5">
+              <h1>Subscribe to our newsletter</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor<br /> enim ipsam voluptatem quia voluptas quia non numquam eius</p>
+              
+              <Form className="d-flex subscribeForm justify-content-center mt-3">
+
+                <Form.Group controlId="mailSubscribe" className="mb-2">
+                  <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>{/* end of Form.Group */}
+
+                <Button type="submit" className="mb-2">Subscribe</Button>
+                
+              </Form>{/* end of Form */}
+            </Card.Body>{/* end of Card.Body */}
+          </Card>{/* end of Card */}
+        </Col>{/* end of Col */}
+      </Row>
+    )
+}
+
+export{
+    NewsLetterComponent
+}
