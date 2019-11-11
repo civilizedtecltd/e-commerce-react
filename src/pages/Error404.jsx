eca5e8b31640d3d73c7cc35de33af58252480665
Nondokishor <mamunahmed678@gmail.com>
2019-11-11 12:14:18 +0600

    Order Pgae fixing

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -4,17 +4,20 @@ import {Link} from 'react-router-dom';
 
 function ErrorPage() {
     return(
+    <div className="errorPage">
         <Container className ="mt-5">
-        <Row>
-            <Col sm={12}>
-                <h1 className='text-center'>404</h1>
-                <h2 className="text-center">Page Not found</h2>
-                <div className="text-center" style={{fontSize:40}}>
-                <Link to="/">GO HOME</Link>
-                </div>
-            </Col>
-        </Row>
-     </Container>
+            <Row>
+                <Col sm={12}>
+                    <h1 className='text-center'>404</h1>
+                    <h2 className="text-center">Page Not found</h2>
+                    <div className="text-center" style={{fontSize:40}}>
+                    <Link to="/">GO HOME</Link>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    </div>
+    
     )
 }
 
