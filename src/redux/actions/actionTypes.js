9ef1b21f2fc1924730373993ed3d01b703b21154
Imrul Kais Khan <imrulkk@outlook.com>
2019-11-24 16:46:26 +0600

    item add to card implemented with redux successfully

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,8 +1,14 @@
-export const FETCH_ALL_BOOKS = 'FETCH_ALL_BOOKS';
-export const SHOW_SINGLE_BOOK = 'SHOW_SINGLE_BOOK';
-export const FETCH_BOOK_BY_CATEGORY = 'FETCH_BOOK_BY_CATEGORY';
-export const FETCH_ALL_CATEGORY = 'FETCH_ALL_CATEGORY';
 
+// Book action types
+export const FETCH_ALL_BOOKS        =   'FETCH_ALL_BOOKS';
+export const SHOW_SINGLE_BOOK       =   'SHOW_SINGLE_BOOK';
+export const FETCH_BOOK_BY_CATEGORY =   'FETCH_BOOK_BY_CATEGORY';
+export const FETCH_ALL_CATEGORY     =   'FETCH_ALL_CATEGORY';
 
+//Cart action types
+export const ADD_TO_CART            =   'ADD_TO_CART';
+export const REMOVE_FROM_CART       =   'REMOVE_FROM_CART';
+export const INCREASE_QUANTITY      =   'INCREASE_QUANTITY';
+export const DECREASE_QUANTITY      =   'DECREASE_QUANTITY';
 
 
