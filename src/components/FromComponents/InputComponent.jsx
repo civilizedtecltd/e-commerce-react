21d35b70d8a16bfe3569db9132132ce05fdc49a2
nondukishor <nipuchakraborty86@hotmail.com>
2020-01-12 17:50:07 +0600

    slect in progress done

--- src/components/FromComponents/InputComponent.jsx
+++ src/components/FromComponents/InputComponent.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
 
-
-//Input From Components
-
 function InputFrom ({ LabelId, TypeName, LabelTitle, Name, Value, Placeholder, ClassName, callback }) {
 
     const handleOnchange = event => {
@@ -16,15 +13,19 @@ function InputFrom ({ LabelId, TypeName, LabelTitle, Name, Value, Placeholder, C
     return (
         <Form.Group className = {ClassName}>
             <Form.Label htmlFor = {LabelId} > {LabelTitle} </Form.Label>
-            <Form.Control type = {TypeName} id = {LabelId} name={Name} defaultValue={Value} placeholder={Placeholder} onChange = {handleOnchange} />
+            <Form.Control type = {TypeName} id = {LabelId} name={Name} value={Value} placeholder={Placeholder} onChange = {handleOnchange} />
         </Form.Group>
     )
 }
 
 //Select From Components
 function SelectFrom({ LabelTitle, category, callback }) {
+    const state = window.localStorage.getItem('state')
+    const category_id = JSON.parse(state).auth.user.category_id;
+     console.log(category_id)
     const handleOnchange = (event) => {
         event.preventDefault();
+   
         callback({
             category_id: event.target.value
         });
@@ -35,10 +36,19 @@ function SelectFrom({ LabelTitle, category, callback }) {
         <Form.Group>
             <Form.Label> {LabelTitle} </Form.Label>
             <Form.Control as="select" onChange = {handleOnchange}>
-                <option defaultValue={"Select Category"}>Select Category</option>
+                <option defaultValue={"Select Category"}> Select Category </option>
                 {
-                    (category === undefined ) ? [] : category.map((element, index) =>
-                        <option key = {index} value = {element.id}>{ element.category }</option>
+                    (category === undefined ) ? [] : category.map((element, index) =>(category_id===element.id) ?(<option selected 
+                        key = {index}
+                        id={index}
+                        value = {element.id}>
+                            { element.category }
+                        </option>) :(<option 
+                        key = {index}
+                        id={index}
+                        value = {element.id}>
+                            { element.category }
+                        </option> )
                     )
                 }
             </Form.Control>
