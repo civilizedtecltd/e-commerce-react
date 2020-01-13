e96e1396aab6a17c9c16bc09de86ed865aba5f41
tawhidjony <tawhid.jony.jhm@gmail.com>
2020-01-13 17:20:51 +0600

    fixed mobile responsive issue

--- src/components/paymentMethodComponent/PaymentMethodComponent.jsx
+++ src/components/paymentMethodComponent/PaymentMethodComponent.jsx
@@ -4,93 +4,93 @@ import NumberFormat from 'react-number-format';
 function PaymentMethodComponent(props) {
   const [showCardAlert, setShowCardAlert] = useState(false);
   const [message, setMessage] = useState("This information is not valid!");
-    const [card, setCard] = useState({})
+  const [card, setCard] = useState({})
 
-    const handleOnChange = (e) => {
-        setCard({
-            ...card,
-            [e.target.name]:e.target.value
-        })
-    }
-
-    const handleOnClick = (e) => {
-      e.preventDefault();
-       if (!card.card_number && !card.mm && !card.yy && !card.ccv) {
-         setMessage("Field should not be empty");
-         setShowCardAlert(true);
-         return setTimeout(() => setShowCardAlert(false), 2000);
-       }
-      if (card.card_number && isNaN(Number(card.card_number[18]))) {
-         setMessage("Card Number should be 16 digit");
-         setShowCardAlert(true)
-        return setTimeout(() => setShowCardAlert(false), 2000);
-      }
-      if (!card.card_number) {
-        setMessage("Card Number should not empty");
-        setShowCardAlert(true);
-        return setTimeout(() => setShowCardAlert(false), 2000);
-      }
-      if (!card.mm) {
-        setMessage("MM should not be empty");
-        setShowCardAlert(true)
-        return setTimeout(() => setShowCardAlert(false), 2000);
-      }
-      if (!card.yy) {
-        setMessage("YY should not be empty");
-        setShowCardAlert(true)
-        return setTimeout(() => setShowCardAlert(false), 2000);
-      }
-      if (!card.ccv) {
-        setMessage("CVV should not be empty");
-        setShowCardAlert(true);
-        return setTimeout(() => setShowCardAlert(false), 2000); 
-      }
-      
-     
-      else if (card.card_number && card.mm && card.yy && card.ccv) {
-             props.callback(card);
-           }              
+  const handleOnChange = (e) => {
+    setCard({
+      ...card,
+      [e.target.name]:e.target.value
+    })
   }
-  
-    //Card Input Format
-    const limit = (val, max) => {
-      if (val.length === 1 && val[0] > max[0]) {
-        val = '0' + val;
+
+  const handleOnClick = (e) => {
+    e.preventDefault();
+    if (!card.card_number && !card.mm && !card.yy && !card.ccv) {
+      setMessage("Field should not be empty");
+      setShowCardAlert(true);
+      return setTimeout(() => setShowCardAlert(false), 2000);
+    }
+    if (card.card_number && isNaN(Number(card.card_number[18]))) {
+      setMessage("Card Number should be 16 digit");
+      setShowCardAlert(true)
+      return setTimeout(() => setShowCardAlert(false), 2000);
+    }
+    if (!card.card_number) {
+      setMessage("Card Number should not empty");
+      setShowCardAlert(true);
+      return setTimeout(() => setShowCardAlert(false), 2000);
+    }
+    if (!card.mm) {
+      setMessage("MM should not be empty");
+      setShowCardAlert(true)
+      return setTimeout(() => setShowCardAlert(false), 2000);
+    }
+    if (!card.yy) {
+      setMessage("YY should not be empty");
+      setShowCardAlert(true)
+      return setTimeout(() => setShowCardAlert(false), 2000);
+    }
+    if (!card.ccv) {
+      setMessage("CVV should not be empty");
+      setShowCardAlert(true);
+      return setTimeout(() => setShowCardAlert(false), 2000);
+    }
+
+
+    else if (card.card_number && card.mm && card.yy && card.ccv) {
+      props.callback(card);
+    }
+  }
+
+  //Card Input Format
+  const limit = (val, max) => {
+    if (val.length === 1 && val[0] > max[0]) {
+      val = '0' + val;
+    }
+
+    if (val.length === 2) {
+      if (Number(val) === 0) {
+        val = '01';
+
+        //this can happen when user paste number
+      } else if (val > max) {
+        val = max;
       }
-
-      if (val.length === 2) {
-        if (Number(val) === 0) {
-          val = '01';
-
-          //this can happen when user paste number
-        } else if (val > max) {
-          val = max;
-        }
-      }
-
-      return val;
     }
 
-    const cardExpiryMonth = (val) => {
-      return limit(val.substring(0, 2), '12');
-    }
+    return val;
+  }
 
-    const cardExpiryYear = (val) => {
-      return val.substring(0,2);
-    }
+  const cardExpiryMonth = (val) => {
+    return limit(val.substring(0, 2), '12');
+  }
+
+  const cardExpiryYear = (val) => {
+    return val.substring(0,2);
+  }
 
   return(
       <Card.Body className="addPaymentCard m-0 p-0">
-        <Row>
-          <Col>
+        <Row className="m-0 p-0">
+          <Col className="p-0">
             <Form className="selectPaymentOption">
               <div className="formRadioGroup bgGray mb-2">
                 <div className="payInfoDetails clearfix">
-                    <div className="m-2">
-                        <Alert show={showCardAlert} variant="danger" onClose={() => setShowCardAlert(false)}  dismissible>
-  <p>{message}</p>
-                        </Alert>
-                    </div>
+                  <div className="m-2">
+                    <Alert show={showCardAlert} variant="danger" onClose={() => setShowCardAlert(false)}  dismissible>
+                      <p>{message}</p>
+                    </Alert>
+                  </div>
                   <div className="cardInfoForm p-3">
                     <Row className="align-items-center">
                       <Col sm="10">
@@ -110,24 +110,14 @@ function PaymentMethodComponent(props) {
                           />
                         </Form.Group>
                       </Col>
-
-                      <Col>
-                        <img
-                            src=""
-                            alt=""
-                        />
-                      </Col>
                     </Row>
 
                     <Row className="align-items-center justify-content-between">
                       <Col sm="4">
                         <Form.Group>
-                          <Form.Label>
-                            Expiry date
-                          </Form.Label>
+                          <Form.Label>Expiry date</Form.Label>
                           <ul className="cardPayFiled d-flex align-items-center justify-content-start">
-                            <li>
-
+                            <li className="width-45">
                               <NumberFormat
                                   id="card-mm"
                                   format={cardExpiryMonth}
@@ -139,7 +129,7 @@ function PaymentMethodComponent(props) {
                               />
                             </li>
                             <li className="cardBl">/</li>
-                            <li>
+                            <li className="width-45">
                               <NumberFormat
                                   id="card-yy"
                                   format={cardExpiryYear}
@@ -156,8 +146,9 @@ function PaymentMethodComponent(props) {
                       </Col>
 
                       <Col sm={{ offset: 4 }}>
-                        <Form.Group>
-                          <Form.Label> CVV </Form.Label>
+
+                        <Form.Group controlId="card-cvv" className="ccv-group">
+                          <Form.Label className="ccv-input"> CVV </Form.Label>
                           <NumberFormat
                               type="text"
                               id="card-cvv"
@@ -168,6 +159,7 @@ function PaymentMethodComponent(props) {
                               onChange={handleOnChange}
                           />
                         </Form.Group>
+
                       </Col>
 
                       <Col sm="2">
