62ee94cbbd7453907a5d291d6374ee210a764953
Arif Uddin <arifunctg@gmail.com>
2021-01-04 23:25:25 +0600

    Update config.js

--- src/constants/config.js
+++ src/constants/config.js
@@ -4,8 +4,8 @@ require('dotenv').config({ path: path.join('/home/dell/Freelancing/book_eCommerc
 
 class APP_URL {
     constructor() {
-        //this.BASE = `https://admin.abookstore.co.ke`;
-        this.BASE = `http://127.0.0.1:3333`;
+        this.BASE = `https://admin.abookstore.co.ke`;
+        //this.BASE = `http://127.0.0.1:3333`;
         this.API = `${this.BASE}/api`;
     }
 
